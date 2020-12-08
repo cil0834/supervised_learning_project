163ba41b33b1ba3dc920372cae9936a7364aa570
Kichlids <kichangsongks@gmail.com>
2020-12-07 20:27:42 -0600

    work in backpropagation

--- neural_network.py
+++ neural_network.py
@@ -5,7 +5,8 @@ import numpy as np
 
 W_default_val = 0.1
 
-data = pd.read_csv('forestfires.csv')
+#data = pd.read_csv('forestfires_nozeros.csv')
+data = pd.read_csv('forestfires_nozeros.csv')
 del data['X']
 del data['Y']
 del data['month']
@@ -14,17 +15,17 @@ del data['day']
 # Inputs: FFMC DMC DC ISI temp RH wind rain	
 # Output: area
 
+weight_default = 0.01
 
 
 class NeuralNetwork:
 
     # layers_and_units: list of units for each layer. ex) [5, 10, 20]
-    def __init__(self, data, network_info, learning_rate, momentum, epoch):
+    def __init__(self, data, network_info, learning_rate, epoch):
 
         self.data = data
         self.epoch = epoch
         self.learning_rate = learning_rate
-        self.momentum = momentum
 
         self.num_layers = len(network_info)
 
@@ -35,7 +36,6 @@ class NeuralNetwork:
         # Result of each unit of each layer
         # Search by [layer][unit]
         self.hidden_unit_values = []
-
         self.hidden_unit_errors = []
 
         # List of weights for a single output unit
@@ -45,8 +45,7 @@ class NeuralNetwork:
         self.predicted_output = 0
 
 
-
-        # Populate unit output list
+        # Populate unit value list
         for i in range(len(network_info)):
             layer = []
             for j in range(network_info[i]):
@@ -55,7 +54,7 @@ class NeuralNetwork:
         # Instantiate hidden unit errors here too
         self.hidden_unit_errors = self.hidden_unit_values
 
-        # Construct small random weights to each of the weights for all hidden units in all layers
+        # Construct random weights for hidden units
         # for each layer...
         for i in range(len(network_info)):
             layer = []
@@ -64,37 +63,31 @@ class NeuralNetwork:
                 unit = []
 
                 if i == 0:
-                    for k in range(8):
-                        unit.append(np.random.uniform(-0.05, 0.05))
+                    for k in range(8 + 1):
+                        unit.append(np.random.uniform(-weight_default, weight_default))
                 else:
                     for k in range(network_info[i - 1]):
-                        unit.append(np.random.uniform(-0.05, 0.05))
+                        unit.append(np.random.uniform(-weight_default, weight_default))
                 
                 layer.append(unit)
             self.hidden_weights.append(layer)
         
-        # Construct small random weights to each of the weights for the output unit (area)
+        # Construct random weights for the output unit
         for i in range(network_info[self.num_layers - 1]):
-            self.output_weights.append(np.random.uniform(-0.05, 0.05))
-        
-        '''
-        # print(f'output weights:')
-        # for i in range(len(self.output_weights)):
-        #     print(self.output_weights[i])
+            self.output_weights.append(np.random.uniform(-weight_default, weight_default))
 
-        # print('\n')
 
-        # print(f'Hidden weights')
-        # for i in range(len(self.hidden_weights)):
-        #     print(f'Layer {i}')
-        #     for j in range(len(self.hidden_weights[i])):
-        #         print(f'Unit {j}')
-        #         for k in range(len(self.hidden_weights[i][j])):
-        #             print(self.hidden_weights[i][j][k])
-        '''
-
-        
     def propagate_input_forward(self, row):
+
+        input_fields = [1, self.sigmoid(row['FFMC']), self.sigmoid(row['DMC']), self.sigmoid(row['DC']), 
+                        self.sigmoid(row['ISI']), self.sigmoid(row['temp']), self.sigmoid(row['RH']), 
+                        self.sigmoid(row['wind']), self.sigmoid(row['rain'])]
+        
+        # input_fields = [1, row['FFMC'], row['DMC'], row['DC'], 
+        #         row['ISI'], row['temp'], row['RH'], 
+        #         row['wind'], row['rain']]
+
+        #print(input_fields)
 
         # for each layer...
         for i in range(len(self.hidden_weights)):
@@ -104,29 +97,16 @@ class NeuralNetwork:
 
                 # Sum using the input fields
                 if i == 0:
-                    x0 = row['FFMC']
-                    x1 = row['DMC']
-                    x2 = row['DC']
-                    x3 = row['ISI']
-                    x4 = row['temp']
-                    x5 = row['RH']
-                    x6 = row['wind']
-                    x7 = row['rain']
-
-                    sum = x0 * self.hidden_weights[i][j][0] + \
-                            x1 * self.hidden_weights[i][j][1] + \
-                            x2 * self.hidden_weights[i][j][2] + \
-                            x3 * self.hidden_weights[i][j][3] + \
-                            x4 * self.hidden_weights[i][j][4] + \
-                            x5 * self.hidden_weights[i][j][5] + \
-                            x6 * self.hidden_weights[i][j][6] + \
-                            x7 * self.hidden_weights[i][j][7]
+                    for index in range(8 + 1):
+                        sum += input_fields[index] * self.hidden_weights[i][j][index]
+                    
                 # Sum using the hidden units 
                 else:
+                    # for each weight in a unit...
                     for weight in range(len(self.hidden_weights[i][j])):
+                        # sum(w * x)
                         sum += self.hidden_weights[i][j][weight] * self.hidden_unit_values[i-1][weight]
 
-                
                 self.hidden_unit_values[i][j] = self.sigmoid(sum)
 
         # Compute the predicted output unit
@@ -135,27 +115,48 @@ class NeuralNetwork:
             sum += self.hidden_unit_values[self.num_layers - 1][i] * self.output_weights[i]
         self.predicted_output = self.sigmoid(sum)
 
+        # print(self.predicted_output)
+        # print(self.de_sigmoid(self.predicted_output))
+
+        return self.predicted_output
+
+
+
     def backpropagate_errors(self, row):
+        #print(row['area'])
+        #print(self.predicted_output)
+        output_error = self.predicted_output * (1 - self.predicted_output) * (self.sigmoid(row['area']) - self.predicted_output)
 
-        output_error = self.predicted_output * (1 - self.predicted_output) * (row['area'] - self.predicted_output)
-
-
-        # Construct errors for the hidden units
+        # Calculate errors for the hidden units
 
         # Iterate layers backwards 
         for i in range(self.num_layers - 1, -1, -1):
-            # for each units...
+            #for each units...
             for j in range(len(self.hidden_unit_values[i])):
                 hidden_unit_error = 0
+                # The layer is the last hidden layer - update from output unit
                 if i == self.num_layers - 1:
-                    
                     hidden_unit_error = self.hidden_unit_values[i][j] * (1 - self.hidden_unit_values[i][j]) * self.output_weights[j] * output_error
+                # The layer isn't the last hidden layer - update from other units
                 else:
+                    # for each units in the next layer...
                     for k in range(len(self.hidden_unit_values[i+1])):
-                        hidden_unit_error += self.hidden_weights[i+1][k][j] * self.hidden_unit_errors[i+1][j]
-                    hidden_unit_error *= self.hidden_unit_values[i][j]
+                        hidden_unit_error += self.hidden_weights[i+1][k][j] * self.hidden_unit_errors[i+1][k]
+
+                    hidden_unit_error *= (self.hidden_unit_values[i][j] * (1 - self.hidden_unit_values[i][j]))
                 
                 self.hidden_unit_errors[i][j] = hidden_unit_error
+
+
+        # input_fields = [1, self.sigmoid(row['FFMC']), self.sigmoid(row['DMC']), self.sigmoid(row['DC']), 
+        #                 self.sigmoid(row['ISI']), self.sigmoid(row['temp']), self.sigmoid(row['RH']), 
+        #                 self.sigmoid(row['wind']), self.sigmoid(row['rain'])]
+
+        input_fields = [1, row['FFMC'], row['DMC'], row['DC'], 
+        row['ISI'], row['temp'], row['RH'], 
+        row['wind'], row['rain']]
+
+        # Update the weights
         
         # for each layer..
         for i in range(self.num_layers):
@@ -163,10 +164,11 @@ class NeuralNetwork:
             for j in range(len(self.hidden_weights[i])):
                 # for each weights..
                 for k in range(len(self.hidden_weights[i][j])):
-                    self.hidden_weights[i][j][k] += self.learning_rate * self.hidden_unit_errors[i][j] * self.hidden_unit_values[i][j]
+                    if i == 0:
+                        self.hidden_weights[i][j][k] += self.learning_rate * self.hidden_unit_errors[i][j] * input_fields[k]
+                    else:
+                        self.hidden_weights[i][j][k] += self.learning_rate * self.hidden_unit_errors[i][j] * self.hidden_unit_values[i-1][k]
         
-
-
     def train(self):
 
         num_epoch = 0
@@ -174,56 +176,58 @@ class NeuralNetwork:
         while (num_epoch < self.epoch):
 
             for index, row in self.data.iterrows():
-                self.propagate_input_forward(row)     
+                self.propagate_input_forward(row)    
                 self.backpropagate_errors(row)
+
+            if num_epoch % 100 == 0:
+                print(num_epoch)
             
             num_epoch += 1
-
-
         
-        # print('\n\n')
-        for i in range(len(self.hidden_unit_values)):
-            print(f'Layer {i}')
-            for j in range(len(self.hidden_unit_values[i])):
-                print(f'{self.hidden_unit_values[i][j]}')
-
-# print('\n\n')
-# for i in range(len(self.output_weights)):
-#     print(self.output_weights[i])
-
-# print('\n')
-# print(self.predicted_output)
-
-        
-        # for i in range(len(self.hidden_unit_values)):
+        # for i in range(len(self.hidden_weights)):
         #     print(f'Layer {i}')
-        #     for j in range(len(self.hidden_unit_values[i])):
-        #         print(self.hidden_unit_values[i][j])
+        #     for j in range(len(self.hidden_weights[i])):
+        #         print(f'Unit {j}')
+        #         for k in range(len(self.hidden_weights[i][j])):
+        #             print(self.hidden_weights[i][j][k])
 
+    def test(self):
+
+        # 517 total
+        correct = 0
+
+        deviation = 10
+
+        for index, row in self.data.iterrows():
+
+            prediction = self.propagate_input_forward(row)
+
+            print(f'prediction: {prediction}')
+            #print(f'Actual: {self.sigmoid(row['area'])}')
+
+            print('\n\n Desigmoided')
+            print(f'prediction: {self.de_sigmoid(prediction)}')
+            # area = (float)row['area']
+            # print(f'Actual: {area}')
+
+            return
+            # if abs(prediction - row['area'] < 2):
+            #     correct += 1
         
-
-
-
-
-
-                            
-
-
-
-
-
-
-
-
-        
-
+        #print(f'Accuracy: {correct/315.0}')
     
-    # Activation function
+
     def sigmoid(self, val):
-        return 1 / (1 + math.e ** -val)
+        return (1 / (1 + math.exp(-val)))
     
-    def sigmoid_der(self, val):
-        return sigmoid(val) * (1 - sigmoid(val)) 
+    def de_sigmoid(self, val):
+        return -math.log(1/val - 1)
 
-neural = NeuralNetwork(data, [2, 2], 0.05, 0.05, 100)
+
+
+
+
+neural = NeuralNetwork(data, [8, 8], 0.05, 5000)
 neural.train()
+neural.test()
+
